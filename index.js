2b46831587f8ddd4defad4c35f35e80d0d6fbc58
kapil.gupta <kapil.gupta@girnarsoft.com>
2017-09-24 19:16:44 +0530

    updated index

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 var gulp = require('gulp');
 var webpConvertor = require('./gulpfile');
 
-// Update Folder Paths
+// Update Folder Paths Here according to requirement
 
 var tasks = {
     'webp-dest1': {
@@ -17,10 +17,10 @@ var tasks = {
 
 webpConvertor.init(tasks);
 
-console.log(gulp.tasks)
+//console.log(gulp.tasks)
 
 if (gulp.tasks.webp) { 
-    console.log('gulpfile contains task!');
+    console.log('gulpfile contains webp task!');
     gulp.start('webp', function(f){
     	console.log("Task Completed: " + new Date());
     	
